455d8ccc05d1f2647da18b112f954d06733853e6
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-06-26 09:42:41 +0800

    First Commit
    Date Commit: 6/26/2023
    Time Commit: 9:42 AM

---
+++ others/js/script.js
@@ -0,0 +1,20 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save () {
+    countEl.textContent = 0
+    saveEl.textContent += count + " - "
+    count = 0
+}
+
+function reset() {
+    saveEl.textContent = "Previous Counts: "
+    count = 0
+    countEl.textContent = 0
+}
