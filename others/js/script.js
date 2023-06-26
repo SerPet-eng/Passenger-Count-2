074e2ccbcd7430cab1b1db971b9f84cac05ec393
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-06-26 10:11:09 +0800

    Update script.js
    I add some comments into the script.js for the functions and variables and tell what it does
    
    Date Commit: 6/26/2023
    Time Commit: 10:10 AM

--- others/js/script.js
+++ others/js/script.js
@@ -1,18 +1,22 @@
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el") //To get the count element
+let saveEl = document.getElementById("save-el")   //To get the save element
 let count = 0
 
+
+//This will "increment" or add 1 number to the HTML document that has an id of "count-el"
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+//This will "save" the count number and display it into the HTML document that has an id of "count-el"
 function save () {
     countEl.textContent = 0
     saveEl.textContent += count + " - "
     count = 0
 }
 
+//This will "reset" all the numbers and text in the HTML documents
 function reset() {
     saveEl.textContent = "Previous Counts: "
     count = 0
